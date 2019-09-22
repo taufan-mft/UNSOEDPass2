36aa86e5124297fa8ef07c53d6a3c81616a14c8a
Taufan <taufanderazor@gmail.com>
2019-09-22 17:10:33 +0700

    added act sinkronisasi
    untuk pilih hari

--- app/src/main/java/com/topanlabs/unsoedpass/sinkronisasi.java
+++ app/src/main/java/com/topanlabs/unsoedpass/sinkronisasi.java
@@ -2,13 +2,55 @@ package com.topanlabs.unsoedpass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class sinkronisasi extends AppCompatActivity {
-
+    Button btnSimpan, btnMin, btnPlus;
+    TextView txthari;
+    Integer hari;
+    SharedPreferences mSettings;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinkronisasi);
+        btnSimpan = findViewById(R.id.btnSimpan);
+        btnMin = findViewById(R.id.btnMin);
+        btnPlus = findViewById(R.id.btnPlus);
+        txthari = findViewById(R.id.txtHari);
+        mSettings = getSharedPreferences("Settings", 0);
+        editor = mSettings.edit();
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            hari = Integer.parseInt(txthari.getText().toString());
+            hari = hari + 1;
+            txthari.setText(hari.toString());
+            }
+
+        });
+
+        btnMin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                hari = Integer.parseInt(txthari.getText().toString());
+                if (hari != 1) {
+                    hari = hari - 1;
+                    txthari.setText(hari.toString());
+                }
+            }
+
+        });
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               editor.putInt("intSinkron", hari);
+               editor.apply();
+                }
+
+
+        });
     }
 }
